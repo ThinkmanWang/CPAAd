c63596bc348c8fd66f75309ea342ffff7aa65eb5
Thinkman Wang <wangxf1985@gmail.com>
2018-12-06 17:42:11 +0800

    update

--- PositionTest/src/main/java/com/thinkman/positiontest/PositionTest.java
+++ PositionTest/src/main/java/com/thinkman/positiontest/PositionTest.java
@@ -1,12 +1,22 @@
 package com.thinkman.positiontest;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.JsResult;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.thinkman.thinkutils.ThinkLog;
 import com.thinkman.thinkwebviewutils.javascript.WebScreenInfoCollector;
@@ -21,9 +31,16 @@ public class PositionTest extends AppCompatActivity {
     @BindView(R.id.wv_main)
     public ThinkWebView m_wvMain;
 
+    public static final String URL = "http://i.hao61.net/jump/pejt-1";
+
+    Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_position_test);
 
         ButterKnife.bind(this);
@@ -31,11 +48,11 @@ public class PositionTest extends AppCompatActivity {
         ThinkLog.debug("THINKMAN", "Hello World");
 
         initWebView();
-        m_wvMain.loadUrl("http://i.hao61.net/jump/pejt-1");
+        m_wvMain.loadUrl(URL);
     }
 
     private void initWebView() {
-        this.m_wvMain.setWebViewClient(new ThinkWebViewClient(this));
+        this.m_wvMain.setWebViewClient(new MyViewClient(this));
 
         this.m_wvMain.setWebChromeClient(new WebChromeClient() {
             public void onHideCustomView() {}
@@ -74,5 +91,63 @@ public class PositionTest extends AppCompatActivity {
         }
 
         this.m_wvMain.addJavascriptInterface(new WebScreenInfoCollector(), "screenInfoCollector");
+    }
+
+    class MyViewClient extends WebViewClient {
+        public MyViewClient(Context paramContext) {
+
+        }
+
+        public void onLoadResource(WebView paramWebView, String paramString) {
+
+        }
+
+        public void onPageFinished(WebView paramWebView, String paramString) {
+            super.onPageFinished(paramWebView, paramString);
+
+            if (true) {
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        WebScreenInfoCollector.collectScreenInfoJs(m_wvMain, new WebScreenInfoCollector.OnWebScreenInfoListener() {
+                            @Override
+                            public void onGetScreenInfo(int nWidth, int nHeight) {
+                                ThinkLog.debug("THINKMAN", "[" + nWidth + ":" + nHeight + "]");
+                            }
+                        });
+                    }
+                }, 1000);
+            }
+        }
+
+        public void onPageStarted(WebView paramWebView, String paramString, Bitmap paramBitmap) {
+            ThinkLog.debug("THINKMAN", paramString);
+        }
+
+        public void onReceivedError(WebView paramWebView, int paramInt, String paramString1, String paramString2) {
+            super.onReceivedError(paramWebView, paramInt, paramString1, paramString2);
+        }
+
+        public void onReceivedSslError(WebView paramWebView, SslErrorHandler paramSslErrorHandler, SslError paramSslError)
+        {
+            paramSslErrorHandler.proceed();
+        }
+
+        public WebResourceResponse shouldInterceptRequest(WebView paramWebView, WebResourceRequest paramWebResourceRequest)
+        {
+            return super.shouldInterceptRequest(paramWebView, paramWebResourceRequest);
+        }
+
+        public boolean shouldOverrideUrlLoading(WebView paramWebView, WebResourceRequest paramWebResourceRequest)
+        {
+            return super.shouldOverrideUrlLoading(paramWebView, paramWebResourceRequest);
+        }
+
+        public boolean shouldOverrideUrlLoading(WebView paramWebView, String paramString) {
+            return false;
+        }
+
+
+
     }
 }
--- ThinkWebViewUtils/src/main/java/com/thinkman/thinkwebviewutils/javascript/WebScreenInfoCollector.java
+++ ThinkWebViewUtils/src/main/java/com/thinkman/thinkwebviewutils/javascript/WebScreenInfoCollector.java
@@ -1,50 +1,61 @@
 package com.thinkman.thinkwebviewutils.javascript;
 
 import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebScreenInfoCollector {
 
-    public static int sScreenInnerHeight;
-    public static int sScreenInnerWidth;
-    public static int sScrollX;
-    public static int sScrollY;
+//    public static int sScreenInnerHeight;
+//    public static int sScreenInnerWidth;
+//    public static int sScrollX;
+//    public static int sScrollY;
 
-    public static String collectScreenInfoJs()
+    public static OnWebScreenInfoListener mListener = null;
+
+    public static void collectScreenInfoJs(WebView webView, OnWebScreenInfoListener listener)
     {
-        return "javascript: (function() {\n" +
+        mListener = listener;
+
+        webView.loadUrl("javascript: (function() {\n" +
                 "    screenInfoCollector.setInnerScreenInfo(document.documentElement.clientWidth, document.documentElement.clientHeight);\n" +
 //                "    alert('clientHeight ' + document.documentElement.clientHeight);\n" +
 //                "    alert('height ' + window.screen.height);\n" +
 //                "    alert('availHeight ' + window.screen.availHeight);\n" +
-                "})()";
+                "})()");
     }
 
-    public static String collectScrollInfoJs()
-    {
-        return "javascript: (function() {\n" +
-                "    var item = document.querySelector('.trade-detail-main');\n" +
-                "    if (item) {\n" +
-                "        screenInfoCollector.setScrollInfo(item.scrollLeft, item.scrollTop);\n" +
-                "    }\n" +
-                "})()";
-    }
-
-    @JavascriptInterface
-    public void setInnerScreenInfo(int paramInt1, int paramInt2)
-    {
-        sScreenInnerWidth = paramInt1;
-        sScreenInnerHeight = paramInt2;
-    }
+//    public static String collectScrollInfoJs()
+//    {
+//        return "javascript: (function() {\n" +
+//                "    var item = document.querySelector('.trade-detail-main');\n" +
+//                "    if (item) {\n" +
+//                "        screenInfoCollector.setScrollInfo(item.scrollLeft, item.scrollTop);\n" +
+//                "    }\n" +
+//                "})()";
+//    }
 
     @JavascriptInterface
-    public void setScrollInfo(int paramInt1, int paramInt2)
+    public void setInnerScreenInfo(int nWidth, int nHeight)
     {
-        sScrollX = paramInt1;
-        sScrollY = paramInt2;
+//        sScreenInnerWidth = paramInt1;
+//        sScreenInnerHeight = paramInt2;
+
+        if (null == mListener) {
+            return;
+        }
+
+        mListener.onGetScreenInfo(nWidth, nHeight);
     }
+
+//    @JavascriptInterface
+//    public void setScrollInfo(int paramInt1, int paramInt2)
+//    {
+//        sScrollX = paramInt1;
+//        sScrollY = paramInt2;
+//    }
 
     public interface OnWebScreenInfoListener {
-        void onInnerScreenInfo(int nWidth, int nHeight);
-        void onScrollInfo(int nScrollX, int nScrollY);
+        void onGetScreenInfo(int nWidth, int nHeight);
     }
 }
